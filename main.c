1709f6346d50ae885f7779b9f5add3ba0fc2f472
Mohammed Merabet <33690623+mmerabet42@users.noreply.github.com>
2018-10-29 02:55:00 +0100

    Adding a test file

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
   t_list  *matches = NULL;
   int     ret;
   
-  // A bracket is either '(...)', '[...]' or '{...}' each one of them is tested by a rule.
+  // A bracket is either '(...)', '[...]' or '{...}' each one of them is tested by a respective rule.
   // And this rule test the 3 of them.
   ft_regex(RGX_ADD | RGX_ID, "BRACKET", "?[?[@BRACKET1]|?[@BRACKET2]|?[@BRACKET3]@or]", NULL, -2);
   
