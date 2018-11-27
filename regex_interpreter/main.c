c7350e3f5650ac371fd3dab08059cfc4b3866f45
Mohammed Merabet <mmerabet@student.42.fr>
2018-11-27 15:45:51 +0100

    A generic parser

--- regex_interpreter/main.c
+++ regex_interpreter/main.c
@@ -4,6 +4,9 @@
 #include "libft/includes/ft_printf.h"
 #include "libft/includes/ft_list.h"
 
+/*
+** The usage is printed if the required argument are not provided.
+*/
 #define USAGE_STR "Usage: ./regex [-sm] subject [regex] file ...\n"
 
 static int	getoptions(int argc, char ***argv);
@@ -14,30 +17,28 @@ int main(int argc, char **argv)
 	char	*subject;
 	char	*regex;
 
-	/*
-	** Parse the options
-	*/
+	/* Parse the options. */
 	if ((options = getoptions(argc, &argv)) == -1)
 		return (1);
+	/* The subject string comes first, or after the options, if any.  */
 	subject = *(argv++);
+	/* If no arguments after the subject string, then the usage isn't respected, we exit. */
 	if (!*argv)
 	{
 		ft_printf(USAGE_STR);
 		return (1);
 	}
 
+	/* If the options '-m' is given (options & 2), then the regex is the next argument. */
 	if (options & 2)
 		regex = *(argv++);
+	/* Otherwise use the 'MAIN' rule declared in one of the files. */
 	else
 		regex = "?[@MAIN]";
-	/* 
-	** Import all the files.
-	*/
+	/* If files are provided, import them. */
 	while (*argv)
 	{
-		/*
-		** If the importation of a file failed, we clean all the already imported files and exit.
-		*/
+		/* If the importation of a file failed, we clean all the already imported files and exit. */
 		if (ft_regex(RGX_IMPORT, *argv, NULL) == -1)
 		{
 			ft_regex(RGX_CLEAN, NULL, NULL);
@@ -46,11 +47,10 @@ int main(int argc, char **argv)
 		}
 		++argv;
 	}
+	/* If the '-m' is absent, */
 	if (!(options & 2))
 	{
-		/*
-		** Check if the 'MAIN' rule exists, if not, exit.
-		*/
+		/* We check if the 'MAIN' is declared, if it is not, we exit. */
 		void *main_ptr = NULL;
 		ft_regex(RGX_GETRULE, "MAIN", NULL, &main_ptr);
 		if (!main_ptr)
@@ -58,54 +58,63 @@ int main(int argc, char **argv)
 			ft_printf("regex: 'MAIN' rule not found\n");
 			return (1);
 		}
+		/* Set the 'MAIN' rule 'transparent' by setting its id to -2. */
 		((t_regex_func *)main_ptr)->id = -2;
 	}
 
 	/*
-	** Execute the regex.
+	** Execute the regex on the subject string.
 	*/
 	t_list *matches = NULL;
 	int n = ft_regex(RGX_GLOBAL, regex, subject, &matches);
 
+	/* Print the matches in a tree form, if the '-s' option is given, */
 	if (options & 1)
 		ft_print_matches_tree(matches, 0);
+	/* Otherwise print it normally. */
 	else
 		ft_print_matches(subject, matches);
 	ft_printf("\n%d match(es)\n", n);
 
 	/*
-	** Free all allocated memories.
+	** Free the allocated matches, and rules.
 	*/
 	ft_regex(RGX_FREE, NULL, NULL, &matches);
 	ft_regex(RGX_CLEAN, NULL, NULL);
 	return (0);
 }
 
+/* Function that gathers the options */
 static int	getoptions(int argc, char ***argv)
 {
 	t_opt	opt;
 	int		options;
 
+	/* If there is not at least 2 argument, we exit. */
 	if (argc < 3)
 	{
 		ft_printf(USAGE_STR);
 		return (-1);
 	}
-	ft_bzero(&opt, sizeof(t_opt));
+	/* We jump the program name, as the ft_getopt function assumes that it is not a part of the array. */
 	++(*argv);
 	options = 0;
 	while (ft_getopt(argv, "sm", &opt) != OPT_END)
 	{
+		/* If an unknown option is given, we exit. */
 		if (opt.ret == OPT_UNKNOWN)
 		{
 			ft_printf(USAGE_STR);
 			return (-1);
 		}
+		/* If the option 's' is met, then enable the first bit of `options` */
 		if (opt.c == 's')
 			options |= 1;
+		/* If the option 'm' is met, then enable the second bit of `options` */
 		else if (opt.c == 'm')
 			options |= 2;
 	}
+	/* If no arguments after the options, then the usage isn't respected, we exit. */
 	if (!**argv)
 	{
 		ft_printf(USAGE_STR);
